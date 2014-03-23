b52afa5ccdefc4f3bbf3b617417ad44d22f22001
Vitor HP <vitorhp2@gmail.com>
2014-03-23 19:47:17 -0300

    Layout for printing characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+#
+
+skills = [
+  {id:1  , name: "Athletics"    }  ,
+  {id:2  , name: "Burglary"    }  ,
+  {id:3  , name: "Contacts"    }  ,
+  {id:4  , name: "Crafts"      }  ,
+  {id:5  , name: "Deceive"     }  ,
+  {id:6  , name: "Drive"       }  ,
+  {id:7  , name: "Empathy"     }  ,
+  {id:8  , name: "Fight"       }  ,
+  {id:9  , name: "Investigate" }  ,
+  {id:10 , name: "Lore"        }  ,
+  {id:11 , name: "Notice"      }  ,
+  {id:12 , name: "Physique"    }  ,
+  {id:13 , name: "Provoke"     }  ,
+  {id:14 , name: "Rapport"     }  ,
+  {id:15 , name: "Resources"   }  ,
+  {id:16 , name: "Shoot"       }  ,
+  {id:17 , name: "Stealth"     }  ,
+  {id:18 , name: "Will"        }
+]
+
+skills.each do |skill|
+  Skill.create skill
+end
