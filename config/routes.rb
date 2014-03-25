08ad8d6d7de54509985f11d656a74629458acb76
Vitor HP <vitorhp2@gmail.com>
2014-03-25 01:30:20 -0300

    Ladder translation updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Fate::Application.routes.draw do
     devise_for :users, skip: :omniauth_callbacks
 
     resources :characters
+
+    root to: 'characters#index'
   end
 
   devise_for :users, only: :omniauth_callbacks, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get '/:locale' => 'characters#index'
-  root to: 'characters#index'
 end
