8b31448419f4387921ec22a9879090ad1b71bbe8
Vitor HP <vitorhp2@gmail.com>
2014-03-25 01:16:11 -0300

    Devise Translations and login with locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def default_url_options options = {}
-    {
+  def self.default_url_options options = {}
+    options.merge({
       locale: I18n.locale
-    }
+    })
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Fate::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   scope "/(:locale)", locale: /en|pt-BR/ do
+    devise_for :users, skip: :omniauth_callbacks
+
     resources :characters
   end
+
+  devise_for :users, only: :omniauth_callbacks, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get '/:locale' => 'characters#index'
   root to: 'characters#index'
