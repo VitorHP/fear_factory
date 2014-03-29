a85574c41528b9168babc82f84af79ad185c0848
Vitor HP <vitorhp2@gmail.com>
2014-03-29 14:44:29 -0300

    Assigning all characters to a default campaign

--- db/migrate/20140323045616_add_campaign_id_to_character.rb
+++ db/migrate/20140323045616_add_campaign_id_to_character.rb
@@ -1,5 +1,13 @@
 class AddCampaignIdToCharacter < ActiveRecord::Migration
   def change
     add_column :characters, :campaign_id, :integer
+
+    Character.find_each do |c|
+      user = c.user
+      campaign = Campaign.vanilla_fate_core
+      campaign.user = user
+      campaign.characters = user.characters
+      campaign.save
+    end
   end
 end
