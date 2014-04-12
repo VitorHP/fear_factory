5b30bf9f97062ff680fb98a963af690eefa4a0f2
Vitor HP <vitorhp2@gmail.com>
2014-04-12 00:29:13 -0300

    More translations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,4 +4,8 @@ class Skill < ActiveRecord::Base
   has_many :stress_tracks
   has_many :consequences
   belongs_to :skill_group
+
+  def name
+    I18n.t("activerecord.attributes.skill.#{self['name'].parameterize.underscore}", default: self['name'])
+  end
 end
