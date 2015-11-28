f59ef25fe90038f8dae29436fd815a22ab589f96
Vitor HP Bittencourt <vitorhp2@gmail.com>
2015-11-27 23:29:56 -0200

    Fixed use of jquery-ui-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery.ui.all
+//= require jquery-ui
 //= require jquery_nested_form
 //= require tinymce-jquery
 //= require select2
