d88f7dd4f58198a8816fa7bd97eda88d77e6dcf9
Vitor HP Bittencourt <vitorhp2@gmail.com>
2015-11-29 00:49:27 -0200

    font-url helper for aallll the fonts

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ module Fate
       g.helper_specs false
     end
 
-    # Precompilation of font assets
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
