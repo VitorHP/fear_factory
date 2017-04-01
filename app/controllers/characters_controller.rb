20f367de9547e5fcc47e124568a39ed5fb24d4c2
Vitor HP Bittencourt <vitorhp2@gmail.com>
2017-04-01 20:40:24 -0300

    Character#show wip

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -34,7 +34,7 @@ class CharactersController < ApplicationController
   end
 
   def show
-    @character = Character.find params[:id]
+    @character = Presenters::Character.new(Character.find params[:id])
   end
 
   private
