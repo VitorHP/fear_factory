5fd7fbaa8a410236b7020ef2d4f4d43d943c674d
Vitor HP Bittencourt <vitorhp2@gmail.com>
2017-03-31 23:59:37 -0300

    Deleted more unused stuff

--- app/services/build_character.rb
+++ app/services/build_character.rb
@@ -1,60 +1,9 @@
 class BuildCharacter
-  def build options = {}
-    @campaign = options.fetch(:campaign)
-
+  def build_human options = {}
     Character.new do |character|
-      build_aspects character
-      build_stunts character
-      build_extras character
-
-      (1..@campaign.skill_cap).to_a.reverse.each_with_index do |level, index|
-        @campaign.skill_cap.times do
-          character.ratings.build level: level
-        end
+      Skill.each do |s|
+        character.ratings.build skill_id: s.id
       end
-
-      # @campaign.skills.each do |skill|
-      #   character.ratings.build skill: skill
-      # end
-
-      stress_tracks = [
-        {name: 'Físico', skill_id: 12},
-        {name: 'Mental', skill_id: 18}
-      ]
-
-      stress_tracks.reverse.each do |skill|
-        character.stress_tracks.build(name: skill[:name], skill_id: skill[:skill_id]) do |stress_track|
-          [1, 2, 3, 4].each do |level|
-            stress_track.stress_levels.build level: level
-          end
-        end
-      end
-
-      consequences = [
-        {name: 'mild', level: 2},
-        {name: 'moderate', level: 4},
-        {name: 'severe', level: 6},
-        {name: 'mild', level: 2, skill_id: 12, skill_level_to_unlock: 5},
-      ]
-
-      consequences.each do |consequence|
-        character.consequences.build consequence
-      end
-
     end
-  end
-
-  private
-
-  def build_aspects character
-    @campaign.number_of_aspects.to_i.times { character.aspects.build }
-  end
-
-  def build_stunts character
-    @campaign.number_of_initial_stunts.to_i.times { character.stunts.build }
-  end
-
-  def build_extras character
-    3.times { character.extras.build }
   end
 end
